6c88e4824ac5adc4b3822c4303f8c60019ecdaf2
Michael Cardoso <michaelcardoso97@gmail.com>
2025-04-15 07:29:14 -0300

    Criação de models

--- TOUGHTS/index.js
+++ TOUGHTS/index.js
@@ -8,6 +8,9 @@ const app = express();
 
 const conn = require("./db/conn");
 
+const Tought = require("./models/Tought");
+const User = require("./models/User");
+
 app.engine("handlebars", exphbs.engine());
 app.set("view engine", "handlebars");
 
@@ -46,6 +49,7 @@ app.use((req, res, next) => {
 });
 
 conn
+  // .sync({ force: true })
   .sync()
   .then(() => {
     app.listen(3000, () => {
