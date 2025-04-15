2ed519fb40f88c54e913a2e1df6669878aee6c51
Michael Cardoso <michaelcardoso97@gmail.com>
2025-04-15 06:50:33 -0300

    Configuração inicial backend

---
+++ TOUGHTS/index.js
@@ -0,0 +1,57 @@
+const express = require("express");
+const exphbs = require("express-handlebars");
+const session = require("express-session");
+const FileStore = require("session-file-store")(session);
+const flash = require("express-flash");
+
+const app = express();
+
+const conn = require("./db/conn");
+
+app.engine("handlebars", exphbs.engine());
+app.set("view engine", "handlebars");
+
+app.use(express.urlencoded({ extends: true }));
+
+app.use(express.json());
+
+app.use(
+  session({
+    name: "session",
+    secret: "meu_segredo",
+    resave: false,
+    saveUninitialized: false,
+    store: new FileStore({
+      logfn: function () {},
+      path: require("path").join(require("os").tmpdir(), "sessions"),
+    }),
+    cookie: {
+      secure: false,
+      maxAge: 360000,
+      expires: new Date(Date.now() + 360000),
+      httpOnly: true,
+    },
+  })
+);
+
+app.use(flash());
+
+app.use(express.static("public"));
+
+app.use((req, res, next) => {
+  if (req.session.userid) {
+    res.locals.session = req.session;
+  }
+  next();
+});
+
+conn
+  .sync()
+  .then(() => {
+    app.listen(3000, () => {
+      console.log("servidor rodando na porta 3000");
+    });
+  })
+  .catch((err) => {
+    console.log("Erro ao iniciar o servidor", err);
+  });
