044f1a24d2907469133cc8ca3dae2b0b6abc21d9
Michael Cardoso <michaelcardoso97@gmail.com>
2025-04-19 09:25:38 -0300

    Inserindo informações de pensamentos no banco

--- TOUGHTS/routes/toughtsRoutes.js
+++ TOUGHTS/routes/toughtsRoutes.js
@@ -2,8 +2,12 @@ const express = require("express");
 const router = express.Router();
 
 const ToughtController = require("../controllers/ToughtController");
+const checkAuth = require("../helpers/Auth").checkAuth
 
-
+router.get("/add",checkAuth, ToughtController.createTought)
+router.post("/add",checkAuth, ToughtController.createToughtSave)
+router.get("/dashboard",checkAuth, ToughtController.dashboard);
 router.get("/", ToughtController.showThoughts);
+
 
 module.exports = router;
